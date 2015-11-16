24180e4c11546eed14a103938d0026ccb74eea57
Jérôme Moret <jermoret@gmail.com>
2015-11-16 11:42:16 +0100

    Update Audio Player (previous, next, slide, add all columns)

--- Flat5/Flat5/src/main/java/ch/heigvd/flat5/music/model/Music.java
+++ Flat5/Flat5/src/main/java/ch/heigvd/flat5/music/model/Music.java
@@ -1,5 +1,7 @@
 package ch.heigvd.flat5.music.model;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
@@ -8,11 +10,38 @@ import javafx.beans.property.StringProperty;
  */
 public class Music {
     private StringProperty title;
+    private StringProperty artist;
+    private StringProperty album;
+    private StringProperty genre;
+    private StringProperty year;
+    private StringProperty length;
     private String path;
+
+    public Music(String title, String artist, String album, String genre, String year, String length, String path) {
+        this.title = new SimpleStringProperty(title);
+        this.artist = new SimpleStringProperty(artist);
+        this.album = new SimpleStringProperty(album);
+        this.genre = new SimpleStringProperty(genre);
+        this.year = new SimpleStringProperty(year);
+        this.length = new SimpleStringProperty(length);
+        this.path = path;
+    }
 
     public Music(String title, String path) {
         this.title = new SimpleStringProperty(title);
         this.path = path;
+    }
+
+    public String getArtist() {
+        return artist.get();
+    }
+
+    public StringProperty artistProperty() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist.set(artist);
     }
 
     public String getTitle() {
@@ -33,5 +62,53 @@ public class Music {
 
     public void setPath(String path) {
         this.path = path;
+    }
+
+    public String getAlbum() {
+        return album.get();
+    }
+
+    public StringProperty albumProperty() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album.set(album);
+    }
+
+    public String getGenre() {
+        return genre.get();
+    }
+
+    public StringProperty genreProperty() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre.set(genre);
+    }
+
+    public String getLength() {
+        return length.get();
+    }
+
+    public StringProperty lengthProperty() {
+        return length;
+    }
+
+    public void setLength(String length) {
+        this.length.set(length);
+    }
+
+    public String getYear() {
+        return year.get();
+    }
+
+    public StringProperty yearProperty() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year.set(year);
     }
 }
